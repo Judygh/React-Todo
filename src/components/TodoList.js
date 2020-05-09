1c31a3e0cc3cce6d9ab58c3fa6997b833c8efb3a
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-05-09 10:41:28 -0500

    working on todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,29 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoForm = (props) => {
+  //constructor with state
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     item: "",
+  //   };
+  // }
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="item"
+        value={props.item}
+        onChange={props.handleChanges}
+      />
+      <button onClick={props.addItem}>Add Todo</button>
+
+      <button onClick={props.handleClear}>Clear Completed</button>
+    </form>
+  );
+};
+
+export default TodoForm;
